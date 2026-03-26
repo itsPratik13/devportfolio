94d1ae3757b0d9eeef6bc3127546e5d8bce93944
Pratik Survase <survasepratik1304@gmail.com>
2026-03-26 19:35:11 +0530

    tables done

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,8 +3,9 @@ import { Button } from "@workspace/ui/components/button"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh p-6">
-      <ThemeToggle />
+    <div className="flex min-h-svh p-6 ">
+      hello this is  sample text
+     
     </div>
   )
 }
---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+// prisma.config.ts
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    // The CLI uses this URL for migrations
+    url: env("DATABASE_URL"),
+  },
+});
